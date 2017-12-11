d9ea9337d64139aab6af561a7c24ccea9c903e74
Esther Weon <esther.s.weon@gmail.com>
2017-12-11 14:46:10 -0800

    Feedback

--- app.js
+++ app.js
@@ -1,33 +1,21 @@
 console.log("JS Loaded");
 
-
-
 $('#btn').click(function(){
  var value = $('#whichCrypto').val();
- var output = $('#outputValue');
+ var $output = $('#outputValue');
 $.ajax({
     url: "https://api.coindesk.com/v1/bpi/currentprice.json",
     method: "GET",
     success: function(response) {
       for (var currency in response.bpi) {
-  console.log(currency);
-  console.log(response.bpi[currency].rate_float);
 if (currency === value) {
-  output.text(currency + " " + "=" + " " + response.bpi[currency].rate_float)
+  $output.text(`${currency} = ${response.bpi[currency].rate_float)}`;
 }
-
-
-
-
-}
-
-      
+}   
     },
     error: function(info) {
-        console.log(info)
+        console.log('There was an error', info);
     },
     dataType: 'json'
 });
 })
-
-
